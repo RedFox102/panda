bb5e1fd8843159125c35d29624423e3c18bc0697
Bellri <RedFox102@gmail.com>
2023-09-14 21:36:14 -0400

    Switch to MAZDA_PEDALS and don't check CRZ_CTRL anymore

--- board/safety/safety_mazda.h
+++ board/safety/safety_mazda.h
@@ -35,7 +35,6 @@ const SteeringLimits MAZDA_STEERING_LIMITS = {
 const CanMsg MAZDA_TX_MSGS[] = {{MAZDA_LKAS, 0, 8},{MAZDA_LKAS2, 1, 8}, {MAZDA_CRZ_BTNS, 0, 8}, {MAZDA_LKAS_HUD, 0, 8}, {MAZDA_RADAR, 0, 8}, {MAZDA_CRZ_CTRL, 0, 8}, {MAZDA_CRZ_INFO, 0, 8}};
 
 AddrCheckStruct mazda_addr_checks[] = {
-  {.msg = {{MAZDA_CRZ_CTRL,     0, 8, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_CRZ_EVENTS,   0, 8, .expected_timestep = 20000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_CRZ_BTNS,     0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{MAZDA_STEER_TORQUE, 0, 8, .expected_timestep = 12000U}, { 0 }, { 0 }}},
@@ -79,8 +78,8 @@ static int mazda_rx_hook(CANPacket_t *to_push) {
     }
 
     // enter controls on rising edge of ACC, exit controls on ACC off
-    if (addr == MAZDA_CRZ_EVENTS) {
-      bool cruise_engaged = GET_BYTE(to_push, 2) & 0x1U;
+    if (addr == MAZDA_PEDALS) {
+      bool cruise_engaged = GET_BYTE(to_push, 0) & 0x8;
       pcm_cruise_check(cruise_engaged);
     }
 
